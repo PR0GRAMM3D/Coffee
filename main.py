3e4027698676636f7747ab5e9392bd1569b9ec65
PR0GRAMM3D <75435039+PR0GRAMM3D@users.noreply.github.com>
2022-04-30 12:58:09 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+import time
+# variables
+menu = "\nBlack Coffee, Espresso, Latte, Cappucino"
+
+# dialog
+print("Welcome to Snootz Coffee Shop")
+time.sleep(3)
+print("We have the best coffee in town")
+time.sleep(2)
+print("Come on in, we're open for business")
+time.sleep(1)
+print("Do you walk in the Coffee Shop?")
+time.sleep(2)
+answer = input("Please enter 'yes' or 'no'\n\n")
+
+
+#dialog and input
+print("\nWell you were coming in anyway!")
+
+name = input("Whats your name?\n\n")
+
+print("\n\nNice to meet you " + name)
+
+print("Heres the menu:\n")
+
+time.sleep(3)
+
+print("Uh, I cant find it.")
+
+time.sleep(7)
+
+print("Oh!")
+
+time.sleep(2)
+
+print("Here it is!")
+
+time.sleep(2)
+
+print(menu)
+
+coffee = input("\nWhat would you like?\n\n")
+
+print("\n\nOne " + coffee + " coming right up!\n wait here.")
+
+time.sleep(1)
+
+
+print("1%")
+time.sleep(2)
+print("7%")
+time.sleep(3)
+print("28%")
+time.sleep(2)
+print("47%")
+time.sleep(1)
+print("64%")
+time.sleep(1)
+print("87%")
+time.sleep(0.7)
+print("96%")
+time.sleep(0.5)
+print("100%\n\n")
+
+print("Heres your " + coffee + "!\n\n")
+print("You walk out Snootz Coffee Shop...\n\n")
+print("And sip your " + coffee + "...")
